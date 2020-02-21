b1fe02b610e70b86860bf04be130f97529636d01
inhye <pepupx2@naver.com>
2020-02-21 15:41:21 +0900

    수정

--- src/Gittest.java
+++ src/Gittest.java
@@ -4,7 +4,7 @@ public class Gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("안녕하세요111111");
+		System.out.println("안녕하세요로시쿠오네가이시마쓰");
 
 	}
 

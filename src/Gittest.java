fc7b037cf3d08ec49ab3d3002730381a2e1d014c
jwm <anmani@daum.net>
2020-02-21 15:42:59 +0900

    mani2

--- src/Gittest.java
+++ src/Gittest.java
@@ -4,7 +4,7 @@ public class Gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("99999111111");
+		System.out.println("OMG!!!!");
 
 	}
 
